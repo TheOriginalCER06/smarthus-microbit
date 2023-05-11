3b0a30f475c85cd1e2ab2e54f51b411786a424c8
Sebastian Einar Krogh-Leknes <67588680+Sebeinkro@users.noreply.github.com>
2023-05-11 09:43:14 +0200

    Fikset temperatur vises på skjerm

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ minode.onSwitchEvent(ConnName.A0, SwitchEvent.MINODE_SWITCH_EVT_CLOSE, function 
     minode.rgbChooseColor(ConnName.A0, MiNodeColor.MiNode_RGB_COLOR_BLUE)
 })
 minode.onSwitchEvent(ConnName.A0, SwitchEvent.MINODE_SWITCH_EVT_OPEN, function () {
-    minode.rgbChooseColor(ConnName.D13, MiNodeColor.MiNode_RGB_COLOR_RED)
+	
 })
-basic.showNumber(minode.DHTGetTemperature(ConnName.A0, DHTTemStyle.MINODE_DHT_CELSIUS))
+basic.forever(function () {
+    basic.showNumber(minode.DHTGetTemperature(ConnName.D12, DHTTemStyle.MINODE_DHT_CELSIUS))
+})
