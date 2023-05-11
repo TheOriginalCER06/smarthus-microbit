9da1fa4bdd544ceabd2a6c9560184c50d127c783
Sebastian Einar Krogh-Leknes <67588680+Sebeinkro@users.noreply.github.com>
2023-05-11 09:32:58 +0200

    Lagde et lite setup som vi kan teste med for å se om at den fungerer

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+minode.onSwitchEvent(ConnName.A0, SwitchEvent.MINODE_SWITCH_EVT_CLOSE, function () {
+    minode.rgbChooseColor(ConnName.A0, MiNodeColor.MiNode_RGB_COLOR_BLUE)
 })
+minode.onSwitchEvent(ConnName.A0, SwitchEvent.MINODE_SWITCH_EVT_OPEN, function () {
+    minode.rgbChooseColor(ConnName.D13, MiNodeColor.MiNode_RGB_COLOR_RED)
+})
+basic.showNumber(minode.DHTGetTemperature(ConnName.A0, DHTTemStyle.MINODE_DHT_CELSIUS))
